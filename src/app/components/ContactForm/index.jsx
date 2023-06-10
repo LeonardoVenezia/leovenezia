e81a8938ce0074eb44b6fbf026b52b972a83b130
Leonardo Venezia <leonardo.venezia@dualbootpartners.com>
2023-06-10 13:50:27 -0300

    Add socialmedia without link reference

--- src/app/components/ContactForm/index.jsx
+++ src/app/components/ContactForm/index.jsx
@@ -1,6 +1,8 @@
 "use client";
 import styles from "./ContactForm.module.scss";
 import { useForm, ValidationError } from '@formspree/react';
+import Image from "next/image";
+import { SocialIcon } from 'react-social-icons';
 
 const ContactForm = () => {
     const [state, handleSubmit] = useForm("xlekykoy");
@@ -44,6 +46,22 @@ const ContactForm = () => {
                 </form>
             </div>
             <div className={styles.socialContainer}>
+                <div className={styles.rowSocial}>
+                    <SocialIcon url="https://linkedin.com/in/leovenezia" />
+                    <SocialIcon url="https://github.com/jaketrent" />
+                    {/* <Image width={40} height={40} alt="linkedin" src="/icons/linkedin.svg" />
+                    <Image width={40} height={40} alt="github" src="/icons/github.svg" /> */}
+                </div>
+                <div className={styles.rowSocial}>
+                    <SocialIcon url="https://instagram.com/jaketrent" />
+                    <SocialIcon url="https://tiktok.com/jaketrent" />
+                    <SocialIcon url="https://twitter.com/jaketrent" />
+                    <SocialIcon url="https://gityoutubehub.com/jaketrent" />
+                    {/* <Image width={40} height={40} alt="instagram" src="/icons/instagram.svg" />
+                    <Image width={40} height={40} alt="tiktok" src="/icons/tiktok.svg" />
+                    <Image width={40} height={40} alt="twitter" src="/icons/twitter.svg" />
+                    <Image width={40} height={40} alt="youtube" src="/icons/youtube.svg" /> */}
+                </div>
 
             </div>
 
